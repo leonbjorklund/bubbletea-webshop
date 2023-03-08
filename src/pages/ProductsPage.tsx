a33a7d403c78b1b1dc7cfae051a1cc91a1852e64
Leon Björklund <leon.bjorklund@gmail.com>
2023-03-08 10:54:51 +0100

    added some product data

--- src/pages/ProductsPage.tsx
+++ src/pages/ProductsPage.tsx
@@ -1,8 +1,23 @@
+import { products } from "../ProductsData"
 
 export function ProductsPage() {
   return (
     <div>
       <h1>This is product page!</h1>
+      {products.map((product) => (
+        <div key={product.id} style={{width: '200px', margin: '10px', border: '2px solid black'}}>
+          <p>{product.title}</p>
+          <img
+            src={product.image}
+            style={{ height: '100px', width: '100px' }}
+          />
+          <p>{product.price} kr</p>
+          <p>{product.description}</p>
+        </div>
+      ))}
     </div>
   )
 }
+
+
+
