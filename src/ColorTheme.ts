417b9df14d50a3d4bb7d5dea78738a89fa567770
mims <113519976+MimPMC@users.noreply.github.com>
2023-03-13 13:56:12 +0100

    Create ColorTheme.ts

---
+++ src/ColorTheme.ts
@@ -0,0 +1,31 @@
+export default {
+    colors: {
+      transparent: 'transparent',
+      black: '#000',
+      white: '#fff',
+      pink:"#FED3D4",
+      lightBrownText: "#54383E",
+      darkBrownText: "#322528",
+      CartBackgroundYellow: "#FFF9F4",
+      yellowGradient: '#FFF2DD',
+      lightYellow:'#FFF2E5',
+      darkPinkButton: "#AF6969",
+      lightGreenButton: '#ACCDB5',
+
+      cardBackground: '#FFF2E5',
+      matchaCard: '#98D16B',
+      darkGreenButton: '#3A8669',
+      bigMatchaCard: '#A2AA70',
+      yellowCardCircle: '#FEE5BE',
+      strawberryTeaCircle: '#FC6868',
+      pinkCardButton: '#F79DA3',
+      milkTeaCircle: '#E4BC80',
+      yellowButton: '#FEE5BE',
+      darkYellowButton: '#E4BC80',
+
+      chocolateBrown: '#96584F',
+      darkChocolateBubbles: '#412D2D',
+      beige: "#C9A69B",
+      footerBottom: "#412D2D"
+    },
+  }
