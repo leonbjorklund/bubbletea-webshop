23c57b6d2d66dd8bcd0fccc2e2a7683d202891b0
mims <113519976+MimPMC@users.noreply.github.com>
2023-03-13 23:42:05 +0100

    random stuff

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,17 +1,44 @@
-import { Container, HStack } from "@chakra-ui/react";
+import { Button, Container, Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay, useDisclosure } from "@chakra-ui/react";
 import theme from "../DefaultThemes";
 
 export function Footer() {
   return (
     <Container as="footer" sx={footerStyle}>
-      <HStack spacing="1rem">
-      </HStack>
+      <BasicUsage/>
+
+      
+
     </Container>
   );
 }
 
 const footerStyle = {
-  backgroundColor:theme.colors.strawberryTeaCircle,
+  backgroundColor: theme.colors.pink,
   maxWidth: "70rem",
   height: "30rem",
 };
+
+function BasicUsage() {
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  return (
+    <>
+      <Button onClick={onOpen}>Open Modal</Button>
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Modal Title</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+          </ModalBody>
+
+          <ModalFooter>
+            <Button colorScheme='pink' mr={3} onClick={onClose}>
+              Close
+            </Button>
+            <Button variant='ghost'>Secondary Action</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  )
+}
