8cb5c2f0b597b0cde855215b41328be96841adae
mims <113519976+MimPMC@users.noreply.github.com>
2023-03-14 15:55:14 +0100

    footer styling

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,18 +1,46 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Container, Grid, GridItem } from "@chakra-ui/react";
 
 export function Footer() {
   return (
     <Box as="footer" sx={footerStyle}>
+  <Container sx={containerStyle}>
+    <Grid
+      sx={gridStyles}
+    >
+      <GridItem  sx={gridItemStyle} />
+      <GridItem  sx={gridItemStyle}  />
+      <GridItem  sx={gridItemStyle} />
+      <GridItem  sx={gridItemStyle}  />
+    </Grid>
+  </Container>
+</Box>
 
-      
-
-    </Box>
-  );
+  ); 
 }
 
 const footerStyle = {
-  backgroundColor: "darkPinkButton",
+  backgroundColor: "pink",
   maxWidth: "100%",
-  height: "30rem",
+  height: ["31rem", "27rem","16rem"],
 };
+
+const containerStyle = {
+  maxWidth: "70rem",
+  height:"100%",
+  paddingTop: ["2rem", "3rem" ]
+}
+
+const gridStyles = {
+  templateColumns: "repeat(1, 1fr)",
+  gap: ["1rem", "1,5rem","2rem"],
+  gridTemplateColumns: ['repeat(1, 1fr)', 'repeat(2, 1fr)', 'repeat(4, 1fr)'],
+  placeItems:"center",
+
+};  
+
+const gridItemStyle = {
+  backgroundColor: "blue.500",
+  height: ["6rem", "10rem","10rem"],
+  width: "100%"
+}
 
