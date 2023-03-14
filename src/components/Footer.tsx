c51bf880999b7a21b3caa7a2af8871572dd9c9f4
mims <113519976+MimPMC@users.noreply.github.com>
2023-03-14 19:20:51 +0100

    footer styling icons readme

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,20 +1,65 @@
-import { Box, Center, Container, Flex, Grid, GridItem, Icon } from "@chakra-ui/react";
-
+import {
+  Box,
+  Center,
+  Container,
+  Flex,
+  Grid,
+  GridItem, Icon,
+  Text
+} from "@chakra-ui/react";
+import { HiLocationMarker } from "react-icons/hi";
 
 export function Footer() {
   return (
     <Box as="footer" sx={footerStyle}>
       <Container sx={containerStyle}>
         <Grid sx={gridStyles}>
-          <GridItem sx={gridItemStyle} />
-          <GridItem sx={gridItemStyle} />
+          <Flex sx={gridItemStyle}>
+            <Center h="100%" w="20%" bg="">
+              <Icon as={HiLocationMarker} />
+            </Center>
+            <Flex
+              h="100%"
+              w="70%"
+              direction={"column"}
+              justify={"center"}
+              whiteSpace={"nowrap"}
+            >
+              <Text as="address" fontSize="sm">
+                Sverigegatan 112
+              </Text>
+              <Text as="address" fontSize="sm">
+                Göteborg 45762
+              </Text>
+              <Text as="address" fontSize="sm">
+                Sweden
+              </Text>
+            </Flex>
+          </Flex>
           <GridItem sx={gridItemStyle} />
           <Flex sx={gridItemStyle}>
-            <Center h="100%" w="15%" bg="red">
-            <Icon as={faBear} />
-
+            <Center h="100%" w="20%" bg="">
+              <Icon as={HiLocationMarker} />
             </Center>
+            <Flex
+              h="100%"
+              w="70%"
+              direction={"column"}
+              justify={"center"}
+              whiteSpace={"nowrap"}
+            >
+              <Text as="address" fontSize="sm">
+                Sverigegatan 112
+              </Text>
+              <Text as="address" fontSize="sm">
+                Göteborg 45762
+              </Text>
+              <Text as="address" fontSize="sm">
+                Sweden
+              </Text>
+            </Flex>
           </Flex>
+          <Flex sx={gridItemStyle}></Flex>
         </Grid>
       </Container>
     </Box>
@@ -22,6 +67,7 @@ export function Footer() {
 }
 
 const footerStyle = {
+  color: "lightBrownText",
   backgroundColor: "pink",
   maxWidth: "100%",
   height: ["31rem", "27rem", "16rem"],
@@ -35,13 +81,14 @@ const containerStyle = {
 
 const gridStyles = {
   templateColumns: "repeat(1, 1fr)",
-  gap: ["1rem", "1,5rem", "2rem"],
+  gap: ["1rem", "1.5rem", "2rem"],
   gridTemplateColumns: ["repeat(1, 1fr)", "repeat(2, 1fr)", "repeat(4, 1fr)"],
   placeItems: "center",
 };
 
 const gridItemStyle = {
-  backgroundColor: "cardBackground",
+  padding: ".5rem",
+  backgroundColor: "darkGreenButton",
   height: ["6rem", "10rem", "10rem"],
-  width: "100%",
+  width: ["70%", "100%", "100%"],
 };
