5fedc5085887dbb15f71069b4483af9ea3f037fb
Caisa Köhlin <113631025+caisak@users.noreply.github.com>
2023-03-15 23:05:13 +0100

    add image and card

--- src/components/BigProductCard.tsx
+++ src/components/BigProductCard.tsx
@@ -1,26 +1,83 @@
-import { Card, CardHeader, Heading } from "@chakra-ui/react";
+import { Box, Card, CardFooter, CardHeader, Image, Text } from "@chakra-ui/react";
 
 interface BigProductCardProps {
   name: string;
-  price: number;
+  price: string;
   description: string;
   imageUrl: string;
   imageAlt: string;
-  bigCardBG: string;
+  backgroundUrl: string;
+  backgroundAlt: string;
 }
 
 export function BigProductCard({ 
-  name, price, description, imageUrl, imageAlt, bigCardBG,
+  name, price, description, imageUrl, imageAlt, backgroundUrl, backgroundAlt, 
  }: BigProductCardProps) {
+  const roundBG = {
+    backgroundColor: "matchaBackground",
+    height: "13rem",
+    width: "13rem",
+    borderRadius: "50%",
+    mt: "1rem",
+    position: "relative",
+    boxShadow: "1px 5px 5px gray"
+  };
   return (
-    <Card border="2px solid black">
-      <CardHeader>
-        <Heading size='md'>Client Report</Heading>
+    <Card align="center" sx={cardStyle}>
+      <Box sx={roundBG}>
+        <Image sx={imageStyle2} src={backgroundUrl} alt={backgroundAlt} />
+        <Image sx={imageStyle} src={imageUrl} alt={imageAlt} />
+      </Box>
+      <CardHeader align="center">
+        <Text sx={headingStyle}>{name}</Text> 
+        <Text sx={priceStyle}>{price}</Text>
+        <Text sx={textStyle}>{description}</Text>
       </CardHeader>
+      <CardFooter>
+        
+      </CardFooter>
     </Card>
   )
 }
 
+const cardStyle = {
+  height: "100%",
+  width: "100%",
+  boxShadow: "3px 3px 3px gray",
+  backgroundColor: "matchaCard",
+}
+
+const headingStyle = {
+  color: "darkBrownText",
+}
+
+const priceStyle = {
+  color: "darkGreenButton",
+}
+
+const textStyle = {
+  color: "lightBrownText",
+}
+
+const imageStyle = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  objectFit: "cover",
+  width: "35%",
+  zIndex: "300"
+};
+
+const imageStyle2 = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: "100%",
+  height: "100%",
+  zIndex: "100"
+};
 
 
 
