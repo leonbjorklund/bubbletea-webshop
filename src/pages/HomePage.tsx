dd25d91e5049536d5c7004fb304db1e53316436b
Caisa Köhlin <113631025+caisak@users.noreply.github.com>
2023-03-16 15:58:55 +0100

    test

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,10 +1,20 @@
 import { Container, Heading } from "@chakra-ui/react";
+import { BigProductCard } from "../components/BigProductCard";
 
 export function HomePage() {
 
   return (
     <Container as="section" maxWidth="4xl"  my="30px">
       <Heading>This is homepage!</Heading>
+      <BigProductCard
+      name="Matcha Latte"
+      price="$6.50"
+      description="Matcha with oat milk and tapioca pearls"
+      imageUrl="/images/greenboba.png"
+      imageAlt=""
+      backgroundUrl="/images/fancyBackground.png"
+      backgroundAlt=""
+      />
     </Container>
   )
 }
