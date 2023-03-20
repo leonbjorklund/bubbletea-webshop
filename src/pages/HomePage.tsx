a382569941324529e190c72cadc0ae51fde477d2
felicia <feliciawillnas@gmail.com>
2023-03-20 03:21:01 +0100

    base showcase

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,13 +1,45 @@
-import { Box, Container, Heading } from "@chakra-ui/react";
+import { Box, Container, Flex, Heading, SystemStyleObject, Text } from "@chakra-ui/react";
 import { TimeForTeaCard } from "../components/TimeForTeaCard";
 
 export function HomePage() {
   return (
     <Box>
-      <Container as="section" maxWidth="4xl" my="">
-        <Heading>This is homepage!</Heading>
-      </Container>
+
+<Flex sx={background} justifyContent="center" maxWidth="4xl" height="20rem">
+  <Container marginLeft="auto">
+    <Heading
+      fontSize={["2rem", "2.5rem", "3rem"]}
+      mb={[1, 2]}
+      color={"darkBrownText"}
+    >
+      Best boba ever
+    </Heading>
+
+    <Text 
+      fontSize='2xl'
+      color={"darkBrownText"}
+    >
+      In love with React Next
+    </Text>
+
+    <Text 
+      fontSize='lg'
+      color={"darkBrownText"}
+    >
+      In love with React Next
+    </Text>
+  </Container>
+</Flex>
+
+
+
       <TimeForTeaCard></TimeForTeaCard>
     </Box>
   );
 }
+
+const background: SystemStyleObject = {
+  background: 'rgb(255, 240, 221)',
+  backgroundImage: 'radial-gradient(circle, rgba(255,240,221,1) 9%, rgba(254,211,212,1) 100%)'
+};
+
