c7028658166f744d92ce9f3cb95e3e741cba5562
felicia <feliciawillnas@gmail.com>
2023-03-20 03:33:23 +0100

    added buttons

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,12 +1,15 @@
-import { Box, Container, Flex, Heading, SystemStyleObject, Text } from "@chakra-ui/react";
+import { Box, Button, Container, Flex, Heading, Image, Stack, SystemStyleObject, Text } from "@chakra-ui/react";
 import { TimeForTeaCard } from "../components/TimeForTeaCard";
 
 export function HomePage() {
   return (
     <Box>
 
-<Flex sx={background} justifyContent="center" maxWidth="4xl" height="20rem">
-  <Container marginLeft="auto">
+<Flex sx={background}>
+    <Container marginRight="auto">
+    <Image src="path/to/image" alt="Image description" />
+  </Container>
+  <Container>
     <Heading
       fontSize={["2rem", "2.5rem", "3rem"]}
       mb={[1, 2]}
@@ -23,14 +26,23 @@ export function HomePage() {
     </Text>
 
     <Text 
-      fontSize='lg'
+      fontSize='16px'
       color={"darkBrownText"}
     >
-      In love with React Next
+      In love with React Next  In love with React Next  In love with React Next  In love with React Next  In love with React Next  In love with React Next 
+      In love with React Next  In love with React Next  In love with React Next  In love with React Next
     </Text>
-  </Container>
-</Flex>
-
+    <Stack direction='row' spacing={4} align='center'>
+      <Button colorScheme='brown' variant='outline' borderRadius="50px" border="2px" padding="20px">
+        Button
+        </Button>
+        <Button colorScheme='brown' variant='outline' borderRadius="50px" border="2px" padding="20px">
+        Button
+        </Button>
+        
+        </Stack>
+        </Container>
+        </Flex>
 
 
       <TimeForTeaCard></TimeForTeaCard>
@@ -40,6 +52,11 @@ export function HomePage() {
 
 const background: SystemStyleObject = {
   background: 'rgb(255, 240, 221)',
-  backgroundImage: 'radial-gradient(circle, rgba(255,240,221,1) 9%, rgba(254,211,212,1) 100%)'
+  backgroundImage: 'radial-gradient(circle, rgba(255,240,221,1) 9%, rgba(254,211,212,1) 100%)',
+  justifyContent: "center",
+  alignItems: "center",
+  margin: "0",
+  width: "100%",
+  height: "20rem",
 };
 
