b7673dfc5e24626384ac54d862132c28ee01dd49
mims <113519976+MimPMC@users.noreply.github.com>
2023-03-21 11:32:08 +0100

    Update ProductPage.tsx

--- src/pages/ProductPage.tsx
+++ src/pages/ProductPage.tsx
@@ -1,12 +1,14 @@
 import { Container } from "@chakra-ui/react";
 import { useParams } from "react-router-dom";
-import { products } from "../../data";
 import { BigProductCard } from "../components/BigProductCard";
+import { useProduct } from "../ProductContext";
 
 export function ProductPage() {
 
+  const { productList } = useProduct()
+
   const params = useParams();
-  const product = products.find((p) => p.id === params.id);
+  const product = productList.find((p) => p.id === params.id);
 
   if (!product) {
     return <p>Produkten hittades inte...</p>;
