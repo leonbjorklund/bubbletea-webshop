3c4bab00a580e0c06df82f79797798371854c59b
felicia <feliciawillnas@gmail.com>
2023-03-21 02:24:50 +0100

    cleaned up code

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -4,63 +4,43 @@ import { TimeForTeaCard } from "../components/TimeForTeaCard";
 export function HomePage() {
   return (
     <Box sx={boxStyling}>
-
       <Flex sx={gradientBackground}>
         <Container marginRight="auto">
           <Image src="/images/showcaseimg.png" alt="Image of bubble tea" width="auto" height="auto" />
         </Container>
-
         <Container>
-          <Heading
-            fontSize={["2rem", "2.5rem", "3rem"]}
-            color={"lightBrownText"}
-          >
+          <Heading fontSize={["2rem", "2.5rem", "3rem"]} color={"lightBrownText"}>
             Best boba ever
           </Heading>
-
-          <Text 
-            fontSize={["2rem", "2.5rem", "2rem"]}
-            color={"lightBrownText"}
-            fontWeight="bold"
-            
-          >
+          <Text fontSize={["2rem", "2.5rem", "2rem"]} color={"lightBrownText"} fontWeight="bold" display={["none", null, "flex"]}>
             In love with React Next
           </Text>
-
-          <Text 
-            fontSize='16px'
-            color={"lightBrownText"}
-            fontFamily= 'Comfortaa'
-            marginTop="1rem"
-          >
-            In love with React Next  In love with React Next  In love with React Next  In love with React Next  In love with React Next  In love with React Next 
-            In love with React Next  In love with React Next  In love with React Next  In love with React Next
+          <Text fontSize='16px' color={"lightBrownText"} fontFamily='Comfortaa' marginTop="1rem">
+            In love with React Next  In love with React Next  In love with React Next  In love with React Next  In love with React Next  In love with React Next In love with React Next  In love with React Next  In love with React Next  In love with React Next
           </Text>
-
-          <Stack direction='row' spacing={4} align='center' justifyContent="center" marginTop="40px">
+          <Stack direction={['column', null, 'row']} spacing={4} align='center' justifyContent="center" marginTop="40px" display={["none", null, "flex"]}>
             <Button sx={buttonStyling}>
               PRODUCTS
             </Button>
             <Button sx={buttonStyling}>
-              RECIPIES
+              RECIPES
             </Button>
           </Stack>
         </Container>
       </Flex>
       <Image src="/images/crescent.png" alt="" width="auto" height="auto" />
-
       <TimeForTeaCard />
     </Box>
   );
 }
 
 const boxStyling: SystemStyleObject = {
-  marginTop:"5rem",
-}
+  marginTop: "5rem",
+};
 
 const gradientBackground: SystemStyleObject = {
-  background: 'rgb(254,211,212)',
-  backgroundImage: 'linear-gradient(0deg, rgba(254,211,212,1) 0%, rgba(255,241,221,1) 47%, rgba(254,211,212,1) 100%, rgba(255,255,255,1) 100%)',
+  background: 'rgb(254, 211, 212)',
+  backgroundImage: 'linear-gradient(0deg, rgba(254, 211, 212, 1) 0%, rgba(255, 241, 221, 1) 47%, rgba(254, 211, 212, 1) 100%, rgba(255, 255, 255, 1) 100%)',
   width: "100%",
   height: "20rem",
   color: "lightBrownText",
@@ -68,11 +48,11 @@ const gradientBackground: SystemStyleObject = {
 };
 
 const buttonStyling: SystemStyleObject = {
-  colorScheme:'brown',
-  variant:'outline',
-  borderRadius:"50px",
-  border:"2px",
-  padding:"25px",
+  colorScheme: 'brown',
+  variant: 'outline',
+  borderRadius: "50px",
+  border: "2px",
+  padding: "25px",
   fontFamily: 'Comfortaa',
   background: "none",
-}
+};
