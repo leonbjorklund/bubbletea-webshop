9777e51ff2c76f852570b52e984fb97eecb6ad75
Caisa Köhlin <113631025+caisak@users.noreply.github.com>
2023-03-25 23:56:56 +0100

    comment

--- src/components/AdminProducts.tsx
+++ src/components/AdminProducts.tsx
@@ -11,6 +11,7 @@ export function AdminProducts() {
         justify={["center", "center", "center", "space-between"]}
         gap={1}
       >
+        {/* Renders a list of product cards in a grid layout. Displays products managed by an admin */}
         <AdminProductsLayout />
         <Box as="aside" width={["100%", "100%", "96%", "40%"]}>
           <AdminCard />
